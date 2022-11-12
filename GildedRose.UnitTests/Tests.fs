d4173b9d0e1fe1af2764cd16fca88dd38df731c0
Christopher Bimson <christopher.bimson@gmail.com>
2022-11-12 18:08:02 +0000

    New tests for the Appreciating Item.

--- GildedRose.UnitTests/Tests.fs
+++ GildedRose.UnitTests/Tests.fs
@@ -29,7 +29,6 @@ let ``NormalItem reduces quality at double rate after SellIn is 0`` () =
     test <@ -2 = item.SellIn @>
     test <@ 0 = item.Quality @>
 
-
 [<Fact>]
 let ``NormalItem reduces Quality By 1 and SellIn by 1`` () =
     let item = Item.UpdateNormalItem {
@@ -43,7 +42,7 @@ let ``NormalItem reduces Quality By 1 and SellIn by 1`` () =
     test <@ 0 = item.Quality @>
 
 [<Fact>]
-let ``Apprecitating Item increases Quality By 1 and SellIn by 1`` () =
+let ``Appreciating Item increases Quality By 1 and SellIn by 1`` () =
     let item = Item.UpdateItem {
         Name = "Aged Brie"; 
         SellIn = 0; 
@@ -53,3 +52,28 @@ let ``Apprecitating Item increases Quality By 1 and SellIn by 1`` () =
     test <@ "Aged Brie" = item.Name @>
     test <@ -1 = item.SellIn @>
     test <@ 2 = item.Quality @>
+    
+[<Fact>]
+let ``Appreciating Item's quality is capped at 50`` () =
+    let item = Item.UpdateItem {
+        Name = "Aged Brie"; 
+        SellIn = 1; 
+        Quality = 50
+    }
+
+    test <@ "Aged Brie" = item.Name @>
+    test <@ 0 = item.SellIn @>
+    test <@ 50 = item.Quality @>
+    
+[<Fact>]
+let ``Appreciating Item's quality cannot increase above 50`` () =
+    let item = Item.UpdateItem {
+        Name = "Aged Brie"; 
+        SellIn = 1; 
+        Quality = 49
+    }
+
+    test <@ "Aged Brie" = item.Name @>
+    test <@ 0 = item.SellIn @>
+    test <@ 50 = item.Quality @>
+    
